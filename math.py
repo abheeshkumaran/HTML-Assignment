7f286fb44ed8e394e89437910d2948558dbfc611
Abheesh Kumaran <159409978+abheeshkumaran@users.noreply.github.com>
2024-03-23 01:28:53 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,8 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+... print(math.ceil(7.3))
+... print(math.floor(3.3))
+... print(math.fabs(8))
+... print(math.factorial(9))
+... print(math.copysign(9,-8))
